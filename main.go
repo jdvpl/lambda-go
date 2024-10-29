c2824aa8ebec1ecfe1b4162280bc279c77dd3361
jdsuarez <jdsuarez@fgs.co>
2024-10-28 23:00:20 -0500

    creating models and validating variables

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package lambdago
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"jdvpl/awsgo"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,5 +17,17 @@ func main() {
 
 func LambdaExcuter(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
 	awsgo.AwsStart()
+	// Get the secret from the AWS Secret Manager
+	if !ParameterValidation() {
+		fmt.Println("Missing SECRETNAME environment variable")
+		err := errors.New("missing SECRETNAME environment variable")
+		return event, err
+	}
 	return event, nil
+}
+
+func ParameterValidation() bool {
+	var getParameter bool
+	_, getParameter = os.LookupEnv("SECRETNAME")
+	return getParameter
 }
